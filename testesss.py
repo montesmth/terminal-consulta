5da4edf4a069515e88d0b78b00f4bae78393b597
Matheus <plutaozaum@gmail.com>
2026-04-27 15:18:07 -0300

    try add css style

--- testesss.py
+++ testesss.py
@@ -1,7 +1,13 @@
 import streamlit as st
 from supabase import create_client
+import os
 
 st.set_page_config(page_title="Pesquisa de Matrículas", layout="wide")
+
+caminho_css = os.path.join(os.path.dirname(os.path.abspath(__file__)), ".streamlit", "styles.css")
+with open(caminho_css) as f:
+    
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 st.title("🔍 Terminal de Consulta")
 
 @st.cache_resource
